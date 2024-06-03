1f2b1ee16e41b0430eafd39c15e1fbc7b10fa410
Ankit Kokane <ankitkokane90@gmail.com>
2024-06-03 06:35:18 +0530

    fixed build error

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Features from "./_components/Features";
-import Github from "./_components/Github";
 import Home from "./_components/Home";
 import InfoSection from "./_components/InfoSection";
 
